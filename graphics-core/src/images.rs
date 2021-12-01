2f09f2d0f1b4ae05e6aece183e6cb35aab041e5d
Mateo Carreras <mateo.carreras@gmail.com>
2021-11-30 17:41:14 -0800

    set rawimage pixel

--- graphics-core/src/images.rs
+++ graphics-core/src/images.rs
@@ -1,6 +1,7 @@
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::render::Canvas;
 use sdl2::surface::Surface;
+use sdl2::rect::Point as SdlPoint;
 
 pub struct RawImage<'a> {
     canvas: Canvas<Surface<'a>>
@@ -12,5 +13,12 @@ impl RawImage<'_> {
         let canvas = surface.into_canvas()?;
 
         Ok(RawImage { canvas })
+    }
+
+    pub fn set_pixel(&mut self, c: Color, x: i32, y: i32) -> Result<(), String> {
+        self.canvas.set_draw_color(c);
+        self.canvas.draw_point(SdlPoint::new(x, y))?;
+
+        Ok(())
     }
 }
