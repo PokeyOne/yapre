ce473646e7b64e11c84fcc79a2ee7b978359457c
Mateo Carreras <mateo.carreras@gmail.com>
2021-12-02 00:19:04 -0800

    test of png creation and saving. works but does not actually delete the file

--- graphics-core/src/images/tests.rs
+++ graphics-core/src/images/tests.rs
@@ -7,3 +7,24 @@ fn test_generate_random_temp_path() {
 
     assert_ne!(a, b);
 }
+
+#[test]
+fn test_save_image() {
+    let mut image = RawImage::new(256, 256);
+    for x in 0..256 {
+        for y in 0..256 {
+            image.set_pixel(Color::new(x as u8, y as u8, 255, 255), x, y);
+        }
+    }
+    let path = generate_random_temp_path();
+
+    let path_prefix = Path::new(&path).parent().unwrap();
+    std::fs::create_dir_all(path_prefix).unwrap();
+
+    match image.save_image_to_path(&path) {
+        Ok(_) => {},
+        Err(e) => panic!("err: {:?}", e)
+    }
+
+    assert!(Path::new(&path).exists());
+}
