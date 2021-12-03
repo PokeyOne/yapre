d0a3f5507d2473a30afcdee36c2bf1ae19431920
Mateo Carreras <mateo.carreras@gmail.com>
2021-12-02 23:57:06 -0800

    started work on loading obj files into the renderer

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -1,3 +1,9 @@
+// Intentionally not including png related stuff here because there is already
+// a library that handles it and it is best to not include that dependency
+// in every little thing that needs any io.
+
+mod obj;
+
 #[cfg(test)]
 mod tests {
     #[test]
